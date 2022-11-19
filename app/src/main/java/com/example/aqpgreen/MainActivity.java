1787fa49874e6031baedb5c2781262855bb14b35
JoseMiguelVM <jverama@unsa.edu.pe>
2022-11-18 22:38:21 -0500

    Borrando clases y datos innecesarios

--- app/src/main/java/com/example/aqpgreen/MainActivity.java
+++ app/src/main/java/com/example/aqpgreen/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.aqpgreen;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText user, pass;
-    Button btnLogin, btnRegistro;
-    //@SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,27 +20,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentNuevo);
             }
         });
-/*
-        user=(EditText) findViewById(R.id.LnombreUsuarioEdit);
-        pass=(EditText) findViewById(R.id.LcontraseñaEdit);
-        btnLogin=(Button) findViewById(R.id.botonLoginCompleto);
-        //btnRegistro=(Button) findViewById(R.id.botonRegistroCompleto);
-
-        btnLogin.setOnClickListener(this);
-        btnRegistro.setOnClickListener(this);*/
-
     }
-/*
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.botonLoginCompleto:
-                break;
-            case R.id.botonRegistroCompleto:
-                Intent i = new Intent(MainActivity.this, Registro.class);
-                startActivity(i);
-                break;
-        }
-    }*/
 }
 
--- app/src/main/java/com/example/aqpgreen/Registro.java
+++ app/src/main/java/com/example/aqpgreen/Registro.java
@@ -2,59 +2,25 @@ package com.example.aqpgreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class Registro extends AppCompatActivity{
-    EditText userName, email, password;
-    Button btnRegistrarse;
-    DaoUsuario dao;
-    //@SuppressLint("MissingInflatedId")
+public class Registro extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        Button botonLogin = findViewById(R.id.logIn);
-        // Para dirigirse al form de login
-        botonLogin.setOnClickListener(new View.OnClickListener() {
+        Button cambioaRegistro = findViewById(R.id.logIn); // Para el cambio de boton de la parte superior del formulario
+        // Para dirigirse al form de registro
+        cambioaRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentNuevo = new Intent(Registro.this, MainActivity.class);
                 startActivity(intentNuevo);
             }
         });
-/*
-        userName=(EditText) findViewById(R.id.RnombreUsuarioEdit);
-        email=(EditText) findViewById(R.id.Rcorreo);
-        password=(EditText) findViewById(R.id.RcontraseñaEdit);
-        btnRegistrarse=(Button) findViewById(R.id.botonRegistroCompleto);
-
-        btnRegistrarse.setOnClickListener(this);
-        dao=new DaoUsuario(this);*/
     }
-/*
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.botonRegistroCompleto:
-                Usuario user=new Usuario();
-                user.setNombreUsuario(userName.getText().toString());
-                user.setCorreo(email.getText().toString());
-                user.setContraseña(password.getText().toString());
-                //Validar datos
-                if(user.isNull()){
-                    Toast.makeText(this, "ERROR: Campos vacios", Toast.LENGTH_SHORT).show();
-                }else if(dao.insertUsuario(user)){
-                    Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(this, "Usuario registrado previamente", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }*/
 }
