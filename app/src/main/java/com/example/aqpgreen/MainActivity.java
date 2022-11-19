285235d5e948089d3a562e978289b0b4886c43c7
JoseMiguelVM <jverama@unsa.edu.pe>
2022-11-19 08:21:18 -0500

    Agregando activity menu, imagenes para hacer los fragments

--- app/src/main/java/com/example/aqpgreen/MainActivity.java
+++ app/src/main/java/com/example/aqpgreen/MainActivity.java
@@ -12,12 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button cambioaRegistro = findViewById(R.id.singUp); // Para el cambio de boton de la parte superior del formulario
+        Button Login = findViewById(R.id.botonLoginCompleto); // Ingresar a la otra interfaz
+
         // Para dirigirse al form de registro
         cambioaRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentNuevo = new Intent(MainActivity.this, Registro.class);
                 startActivity(intentNuevo);
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, menuOpciones.class);
+                startActivity(i);
             }
         });
     }
---
+++ app/src/main/java/com/example/aqpgreen/menuOpciones.java
@@ -0,0 +1,14 @@
+package com.example.aqpgreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuOpciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_opciones);
+    }
+}
