9e2bac4f9c4913cd8e3738cf65ef42138aa4ff48
JoseAntonio98 <sawm5798@gmail.com>
2022-11-22 16:18:05 -0500

    Actualizado atributos tabla Peticion

--- app/src/main/java/com/example/aqpgreen/database/Peticiones/ConectorPeticionesDB.java
+++ app/src/main/java/com/example/aqpgreen/database/Peticiones/ConectorPeticionesDB.java
@@ -7,12 +7,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class ConectorPeticionesDB extends SQLiteOpenHelper {
 
     public static final String TABLE_NAME = "PETICIONES";
-    public static final String IDPETICION = "idPeticion";
-    public static final String IDUSUARIO = "idUsuario";
+    public static final String IDPETICION = "idpeticion";
+    public static final String IDUSUARIO = "idusuario";
     public static final String CATEGORIA = "categoria";
     public static final String CANTIDAD = "cantidad";
     public static final String ORIGEN = "lugarOrigen";
     public static final String DESCRPCION = "descripcion";
+    public static final String PUNTOS = "puntos";
+    public static final String ESTADO = "estado";
     public static final String FOTOURL = "fotoUrl";
 
     static final String DB_NAME = "aqpgreen.db";
@@ -21,8 +23,11 @@ public class ConectorPeticionesDB extends SQLiteOpenHelper {
     private static final String CREATE_TABLE = "create table " + TABLE_NAME + "(" + IDPETICION
             + " INTEGER PRIMARY KEY AUTOINCREMENT, " + IDPETICION + " TEXT NOT NULL, "
             + IDUSUARIO + " TEXT NOT NULL, " + CATEGORIA + "  TEXT NOT NULL,"
-            + CANTIDAD + "TEXT NOT NULL, " + ORIGEN + "TEXT NOT NULL,"
-            + DESCRPCION + "TEXT NOT NULL," + FOTOURL + "TEXT NOT NULL);";
+            + CANTIDAD + " INTEGER NOT NULL, " + ORIGEN + " TEXT NOT NULL,"
+            + DESCRPCION + " TEXT NOT NULL," + PUNTOS + " INTEGER NOT NULL,"
+            + ESTADO + " INTEGER NOT NULL," + FOTOURL + " TEXT NOT NULL,"
+            + "FOREIGN KEY (" + IDUSUARIO + ") "
+            + "REFERENCES USUARIOS (" + IDUSUARIO + ") ON DELETE CASCADE ON UPDATE NO ACTION);";
 
     public ConectorPeticionesDB(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
