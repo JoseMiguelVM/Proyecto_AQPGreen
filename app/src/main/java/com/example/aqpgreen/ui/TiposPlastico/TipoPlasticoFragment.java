0d7830ec97502c2984b46b0d79edc8c121fac5f8
Rosario Alegre <ralegre@unsa.edu.pe>
2022-11-22 20:43:37 -0500

    Vista de Tipos Plasticos

--- app/src/main/java/com/example/aqpgreen/ui/TiposPlastico/TipoPlasticoFragment.java
+++ app/src/main/java/com/example/aqpgreen/ui/TiposPlastico/TipoPlasticoFragment.java
@@ -10,15 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.aqpgreen.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TipoPlasticoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TipoPlasticoFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "";
     private static final String ARG_PARAM2 = "param2";
 
@@ -27,18 +19,8 @@ public class TipoPlasticoFragment extends Fragment {
     private String mParam2;
 
     public TipoPlasticoFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentTipoPlastico.
-     */
-    // TODO: Rename and change types and number of parameters
     public static TipoPlasticoFragment newInstance(String param1, String param2) {
         TipoPlasticoFragment fragment = new TipoPlasticoFragment();
         Bundle args = new Bundle();
