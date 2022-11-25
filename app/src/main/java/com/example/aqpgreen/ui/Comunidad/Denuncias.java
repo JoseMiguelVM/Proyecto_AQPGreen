c4ed0c50d98769b54f672060fb0e54e1cd017f84
rtitod <rtitod@unsa.edu.pe>
2022-11-25 01:09:58 -0500

    Denuncias can be stored on DB

--- app/src/main/java/com/example/aqpgreen/ui/Comunidad/Denuncias.java
+++ app/src/main/java/com/example/aqpgreen/ui/Comunidad/Denuncias.java
@@ -2,13 +2,20 @@ package com.example.aqpgreen.ui.Comunidad;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.aqpgreen.R;
+import com.example.aqpgreen.database.Denuncias.DenunciaDBController;
+import com.example.aqpgreen.database.Usuarios.AdministradorUsuariosDB;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,7 +23,7 @@ import com.example.aqpgreen.R;
  * create an instance of this fragment.
  */
 public class Denuncias extends Fragment {
-
+    private DenunciaDBController dbManager;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -55,12 +62,35 @@ public class Denuncias extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        dbManager = new DenunciaDBController(getContext());
+        dbManager.open();
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        EditText dni_denunciante_Edit = (EditText) view.findViewById(R.id.DNI_denunciante);
+        EditText nombre_denunciante_Edit = (EditText) view.findViewById(R.id.nombre_denunciante);
+        EditText ubicacion_denunciante_Edit = (EditText) view.findViewById(R.id.ubicacion_denunciante);
+        EditText descripcion_denunciante_Edit = (EditText) view.findViewById(R.id.descripcion_denuncia);
+        Button registrardenuncia_btn = (Button) view.findViewById(R.id.botonRegistrarDenuncia);
+        registrardenuncia_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dni = dni_denunciante_Edit.getText().toString();
+                String nombres = nombre_denunciante_Edit.getText().toString();
+                String ubicacion = ubicacion_denunciante_Edit.getText().toString();
+                String descripcion = descripcion_denunciante_Edit.getText().toString();
+                dbManager.insert(dni, nombres, ubicacion, descripcion);
+                dbManager.close();
+            }
+        });
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_denuncias, container, false);
     }
 }
