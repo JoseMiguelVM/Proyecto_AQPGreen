1e8e053a91f694a088ffe2e6a98083d054d90c7f
JoseAntonio98 <sawm5798@gmail.com>
2022-11-25 11:20:45 -0500

    Implementado boton para regresar ComunidadFragment

--- app/src/main/java/com/example/aqpgreen/ui/Comunidad/ComunidadFragment.java
+++ app/src/main/java/com/example/aqpgreen/ui/Comunidad/ComunidadFragment.java
@@ -13,10 +13,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.aqpgreen.R;
 
 public class ComunidadFragment extends Fragment{
+
+    private ImageButton btn_regresar_fragment;
+    private Button btn_ir_premios;
+    private Button btn_ir_denuncia;
 
     public ComunidadFragment() {
         // Required empty public constructor
@@ -39,22 +44,33 @@ public class ComunidadFragment extends Fragment{
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         final NavController navController = Navigation.findNavController(view);
+        inicializar_elementos(view);
 
-        Button botonPremios = view.findViewById(R.id.Btn_listaDePremios);
-        Button botonDenuncia = view.findViewById(R.id.Btn_denuncias);
-
-        botonPremios.setOnClickListener(new View.OnClickListener() {
+        btn_regresar_fragment.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.recyclerview);
+            public void onClick(View view) {
+                navController.popBackStack();
             }
         });
 
-        botonDenuncia.setOnClickListener(new View.OnClickListener() {
+        btn_ir_premios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.recyclerview);
+            }
+        });
+
+        btn_ir_denuncia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 navController.navigate(R.id.denuncias);
             }
         });
+    }
+
+    public void inicializar_elementos (View view) {
+        btn_regresar_fragment = view.findViewById(R.id.btnIcoAtras);
+        btn_ir_premios = view.findViewById(R.id.Btn_listaDePremios);
+        btn_ir_denuncia = view.findViewById(R.id.Btn_denuncias);
     }
 }
