8104eb994bd1e3b887f754dfba9c1fb9e3b81d8d
rtitod <rtitod@unsa.edu.pe>
2022-11-25 00:01:48 -0500

    Denuncias DB controller

--- app/src/main/java/com/example/aqpgreen/database/Denuncias/ConectorDenunciasDB.java
+++ app/src/main/java/com/example/aqpgreen/database/Denuncias/ConectorDenunciasDB.java
@@ -1,4 +1,40 @@
 package com.example.aqpgreen.database.Denuncias;
 
-public class ConectorDenunciasDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ConectorDenunciasDB  extends SQLiteOpenHelper {
+
+
+    public static final String TABLE_NAME = "DENUNCIAS";
+
+    public static final String IDDENUNCIAS = "iddenuncias";
+    public static final String DNI = "login";
+    public static final String NOMBRES_COMPLETOS = "email";
+    public static final String UBICACION = "password";
+    public static final String DESCRIPCION = "password";
+
+    static final String DB_NAME = "aqpgreen.db";
+
+    static final int DB_VERSION = 1;
+
+    private static final String CREATE_TABLE = "create table " + TABLE_NAME + "(" + IDDENUNCIAS
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DNI + " TEXT NOT NULL, " + NOMBRES_COMPLETOS + " TEXT NOT NULL, " + UBICACION
+            + "  TEXT NOT NULL, " + DESCRIPCION + "  TEXT NOT NULL);";
+
+    public ConectorDenunciasDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 }
--- app/src/main/java/com/example/aqpgreen/database/Denuncias/DenunciaDBController.java
+++ app/src/main/java/com/example/aqpgreen/database/Denuncias/DenunciaDBController.java
@@ -1,4 +1,66 @@
 package com.example.aqpgreen.database.Denuncias;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.aqpgreen.database.Denuncias.DenunciaDBController;
+import com.example.aqpgreen.database.Denuncias.ConectorDenunciasDB;
+
 public class DenunciaDBController {
+    private ConectorDenunciasDB dbHelper;
+
+    private Context context;
+
+    private SQLiteDatabase database;
+
+    public DenunciaDBController(Context c) {
+        context = c;
+    }
+
+    public DenunciaDBController open() throws SQLException {
+        dbHelper = new ConectorDenunciasDB(context);
+        database = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public void insert(String dni, String nombres, String ubicacion, String descripcion) {
+        ContentValues contentValue = new ContentValues();
+        contentValue.put(ConectorDenunciasDB.DNI, dni);
+        contentValue.put(ConectorDenunciasDB.NOMBRES_COMPLETOS, nombres);
+        contentValue.put(ConectorDenunciasDB.UBICACION, ubicacion);
+        contentValue.put(ConectorDenunciasDB.DESCRIPCION, descripcion);
+        database.insert(ConectorDenunciasDB.TABLE_NAME, null, contentValue);
+    }
+
+    public Cursor fetch() {
+        String[] columns = new String[] { ConectorDenunciasDB.DNI, ConectorDenunciasDB.NOMBRES_COMPLETOS, ConectorDenunciasDB.UBICACION,
+                ConectorDenunciasDB.DESCRIPCION, ConectorDenunciasDB.DESCRIPCION};
+        Cursor cursor = database.query(ConectorDenunciasDB.TABLE_NAME, columns, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public int update(long iddenuncia, String dni, String nombres, String ubicacion, String descripcion) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ConectorDenunciasDB.DNI, dni);
+        contentValues.put(ConectorDenunciasDB.NOMBRES_COMPLETOS, nombres);
+        contentValues.put(ConectorDenunciasDB.UBICACION, ubicacion);
+        contentValues.put(ConectorDenunciasDB.DESCRIPCION, descripcion);
+        int i = database.update(ConectorDenunciasDB.TABLE_NAME, contentValues, ConectorDenunciasDB.IDDENUNCIAS + " = " + iddenuncia, null);
+        return i;
+    }
+
+    public void delete(long iddenuncias) {
+        database.delete(ConectorDenunciasDB.TABLE_NAME, ConectorDenunciasDB.IDDENUNCIAS+ "=" + iddenuncias, null);
+    }
+
 }
