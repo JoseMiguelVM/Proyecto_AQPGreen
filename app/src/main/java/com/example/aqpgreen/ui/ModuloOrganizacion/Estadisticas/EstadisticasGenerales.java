0dda88ddc8b22d6340f335c32ae886f882ec2e57
JoseMiguelVM <jverama@unsa.edu.pe>
2022-12-05 20:11:33 -0500

    Menu terminado y creacion de fragments a enlazar

---
+++ app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/Estadisticas/EstadisticasGenerales.java
@@ -0,0 +1,33 @@
+package com.example.aqpgreen.ui.ModuloOrganizacion.Estadisticas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aqpgreen.R;
+
+
+
+public class EstadisticasGenerales extends Fragment {
+
+    public EstadisticasGenerales() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_estadisticas_generales, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/Publicaciones/NoticiasPublicadas.java
@@ -0,0 +1,31 @@
+package com.example.aqpgreen.ui.ModuloOrganizacion.Publicaciones;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aqpgreen.R;
+
+public class NoticiasPublicadas extends Fragment {
+
+    public NoticiasPublicadas() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_noticias_publicadas, container, false);
+    }
+}
