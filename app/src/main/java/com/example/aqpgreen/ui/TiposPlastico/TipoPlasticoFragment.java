bfb22915655fbf617f5d5af18569a3b91a1b99ce
JoseMiguelVM <jverama@unsa.edu.pe>
2022-12-17 18:49:37 -0500

    Mejorando interfaz de tipos de plastico

--- app/src/main/java/com/example/aqpgreen/ui/TiposPlastico/TipoPlasticoFragment.java
+++ app/src/main/java/com/example/aqpgreen/ui/TiposPlastico/TipoPlasticoFragment.java
@@ -23,15 +23,6 @@ import java.util.ArrayList;
 
 public class TipoPlasticoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     // TODO: Declaracion de variables
     private ArrayList<Plastico> listaPlastico;
     private RecyclerView recyclerPlastico;
@@ -40,20 +31,9 @@ public class TipoPlasticoFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListaPersonajesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static TipoPlasticoFragment newInstance(String param1, String param2) {
         TipoPlasticoFragment fragment = new TipoPlasticoFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -61,10 +41,6 @@ public class TipoPlasticoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -81,7 +57,7 @@ public class TipoPlasticoFragment extends Fragment {
         recyclerPlastico= (RecyclerView) view.findViewById(R.id.recyclerView_listaCategorias);
         recyclerPlastico.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        llenarListaPersonajes();
+        llenarListaTipos();
 
         ListaPlasticosAdaptador adapter = new ListaPlasticosAdaptador(listaPlastico);
         recyclerPlastico.setAdapter(adapter);
@@ -96,13 +72,13 @@ public class TipoPlasticoFragment extends Fragment {
 
     }
 
-    private void llenarListaPersonajes() {
+    private void llenarListaTipos() {
         listaPlastico.add(new Plastico("PET o PETE","Botella de gaseosa, botella de refresco, botella de agua mineral, botella de aceite de cocina, cuerda de poliéster.",R.drawable.logo_pet));
         listaPlastico.add(new Plastico("HDPE","Botella de leche, envases de productos de limpieza, detergentes para la ropa, champú y gel de ducha, bolsas de plástico",R.drawable.logo_hdpe));
-        listaPlastico.add(new Plastico("PVC","Tubos y cañerías, cables eléctricos, juguetes",R.drawable.logo_pvc));
-        listaPlastico.add(new Plastico("LDPE o PEDB","Bolsas de basura, film transparente",R.drawable.logo_ldpe));
+        listaPlastico.add(new Plastico("PVC","Tubos y cañerías, cables eléctricos en general, juguetes y accesorios.",R.drawable.logo_pvc));
+        listaPlastico.add(new Plastico("LDPE o PEDB","Bolsas de basura, film transparente, envases para el sector cosmético y el sector sanitario",R.drawable.logo_ldpe));
         listaPlastico.add(new Plastico("PP","Chapas de botellas, envases para almacenar alimentos, sorbetes",R.drawable.logo_pp));
         listaPlastico.add(new Plastico("PS","Envases de comida rápida, vasos descartables, vasitos de yogurt",R.drawable.logo_ps));
-        listaPlastico.add(new Plastico("OTROS","Piezas de coches, CD’s,  DVD’s, Biberones",R.drawable.logo_otros));
+        listaPlastico.add(new Plastico("OTROS","Piezas de coches, CD’s,  DVD’s, Biberones, todo lo considerado fuuera de lo antes mencionado.",R.drawable.logo_otros));
     }
 }
