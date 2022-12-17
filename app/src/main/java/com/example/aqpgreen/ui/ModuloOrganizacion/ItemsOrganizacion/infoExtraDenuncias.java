91a647260c686da556f5db9e16e10d88e55e7a8b
JoseMiguelVM <jverama@unsa.edu.pe>
2022-12-17 12:35:04 -0500

    Activando enlaces de mayor informacion en cada ITEM

--- app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/ItemsOrganizacion/infoExtraDenuncias.java
+++ app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/ItemsOrganizacion/infoExtraDenuncias.java
@@ -2,18 +2,30 @@ package com.example.aqpgreen.ui.ModuloOrganizacion.ItemsOrganizacion;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.aqpgreen.R;
 
 public class infoExtraDenuncias extends Fragment {
+    private Button btncancel;
+    private Button btnok;
 
     public infoExtraDenuncias() {
         // Required empty public constructor
+    }
+
+    public void inicializar_elementos (View view) {
+        btncancel = view.findViewById(R.id.btncancel);
+        btnok = view.findViewById(R.id.btnok);
     }
 
     @Override
@@ -26,5 +38,26 @@ public class infoExtraDenuncias extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_info_extra_denuncias, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = Navigation.findNavController(view);
+        inicializar_elementos(view);
+
+        btncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.informacionOpciones);
+            }
+        });
+
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.revisarDenuncias);
+            }
+        });
     }
 }
