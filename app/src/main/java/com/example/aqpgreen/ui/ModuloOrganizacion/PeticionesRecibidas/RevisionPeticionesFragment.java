be607a93df2c1a81fbde5b3c615742e285aa9e4c
JoseAntonio98 <sawm5798@gmail.com>
2022-12-21 15:46:42 -0500

    Optimizado imports

--- app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/PeticionesRecibidas/RevisionPeticionesFragment.java
+++ app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/PeticionesRecibidas/RevisionPeticionesFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
