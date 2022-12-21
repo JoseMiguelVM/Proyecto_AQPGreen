cf441874953c7b8092860d19f505ff459d5d1587
JoseMiguelVM <jverama@unsa.edu.pe>
2022-12-21 00:08:39 -0500

    Agregando funcionalidad de vista de denuncias

--- app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/DenunciasRecibidas/RevisarDenuncias.java
+++ app/src/main/java/com/example/aqpgreen/ui/ModuloOrganizacion/DenunciasRecibidas/RevisarDenuncias.java
@@ -1,16 +1,46 @@
 package com.example.aqpgreen.ui.ModuloOrganizacion.DenunciasRecibidas;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.aqpgreen.R;
+import com.example.aqpgreen.database.Denuncias.DenunciaDBController;
+import com.example.aqpgreen.modelo.Denuncia;
+import com.example.aqpgreen.modelo.ListaDenunciaAdaptador;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RevisarDenuncias extends Fragment {
+    private DenunciaDBController db_peticiones;
+    private SharedPreferences preferencias;
+    private FloatingActionButton btn_crear_peticion_fragment;
+    private ImageButton btn_regresar_fragment;
+    private ProgressBar pb_icono_carga;
+    private TextView tv_aviso_vacio;
 
     public RevisarDenuncias() {
         // Required empty public constructor
@@ -27,5 +57,64 @@ public class RevisarDenuncias extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_revisar_denuncias, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        db_peticiones = new DenunciaDBController(getContext());
+        final NavController navController = Navigation.findNavController(view);
+        inicializar_elementos(view);
+
+        btn_regresar_fragment.setOnClickListener(view1 -> navController.popBackStack());
+
+        generar_recyclerView (view);
+    }
+
+    public void inicializar_elementos (View view) {
+        btn_regresar_fragment = view.findViewById(R.id.btnIcoAtras);
+        pb_icono_carga = view.findViewById(R.id.pb_icono_carga);
+        tv_aviso_vacio = view.findViewById(R.id.tv_lista_vacia);
+
+        preferencias = getContext().getSharedPreferences("var_sesion", Context.MODE_PRIVATE);
+        //SharedPreferences.Editor editor_preferencias = preferencias.edit();
+
+    }
+
+    private void generar_recyclerView(View view) {
+
+        ExecutorService hilo = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        List<Denuncia> lista_denuncias = new ArrayList<>();
+
+        hilo.execute(() -> {
+            db_peticiones.open();
+            Cursor cursor = db_peticiones.fetch();
+            if(cursor.getCount() != 0){
+                try{
+                    for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                        lista_denuncias.add(new Denuncia(cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
+                                cursor.getString(3), cursor.getString(4)));
+                    }
+                } catch (Exception e){
+                    Log.e("ListaPetOrgFragment", e.toString());
+                } finally {
+                    cursor.close();
+                }
+            }
+            else {
+                //Log.e("ListaPetCursorelse", "No hay registros");
+                tv_aviso_vacio.setText("No hay denuncias");
+            }
+            db_peticiones.close();
+
+            handler.post(() -> {
+                pb_icono_carga.setVisibility(View.GONE);
+                ListaDenunciaAdaptador adaptor = new ListaDenunciaAdaptador(lista_denuncias);
+                RecyclerView recyclerView = view.findViewById(R.id.recycleView_listaDenuncias);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+                recyclerView.setAdapter(adaptor);
+            });
+        });
     }
 }
