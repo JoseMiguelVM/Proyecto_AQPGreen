2abd75ab8ac423886c7c7fc58fb58897d2714545
JoseMiguelVM <jverama@unsa.edu.pe>
2022-12-23 10:19:19 -0500

    Vista ONG terminada

--- app/src/main/java/com/example/aqpgreen/ui/Comunidad/ListaDeOng.java
+++ app/src/main/java/com/example/aqpgreen/ui/Comunidad/ListaDeOng.java
@@ -60,17 +60,12 @@ public class ListaDeOng extends Fragment {
 
 
     private void llenarListaONG() {
-        listaONG.add(new ONG("Árbol Pequeño", "Usted puede canjear este premio y poder plantarlo en su hogar, para asi promover el plantio de mas areas verdes.", R.drawable.arbol));
-        listaONG.add(new ONG("Gorra y Polo", "Estos premios reflejan su compromiso con la contaminacion ambiental.", R.drawable.gorra));
-        listaONG.add(new ONG("Mochila", "Es uno de los premios mas bellos, con este podras llevar todo el potencial clasico de las mochilas urbanas.", R.drawable.mochila));
-        listaONG.add(new ONG("Ropa para Perros", "Este premio es ideal para los consentidos de la casa y la moda que pueden llevar nuestros bebes caninos.", R.drawable.ropaperros));
-        listaONG.add(new ONG("Ropa para Gatos", "Este premio es ideal para los consentidos de la casa y la moda que pueden llevar nuestros bebes gatunos.", R.drawable.ropagatos));
-        listaONG.add(new ONG("Realizar Donación", "El premio mas importante, usted podra donar sus puntos acumulados a alguna organizacion benefica", R.drawable.donacion));
-        listaONG.add(new ONG("Ricocan (8KG)", "Ricocan Adulto Multisabores Todas las Razas, el mejor alimento para el engreido de casa.", R.drawable.ricocan));
-        listaONG.add(new ONG("Ricocat (9KG)", "Ricocat Adultos Atun, Sardina Y Trucha, el mejor alimento para el engreido de casa.", R.drawable.ricocat));
-        listaONG.add(new ONG("Patines Eléctricos", "Una moda ecológica y eficiente. Es fácil de conducir y plegar, no requiere mantenimiento y es sostenible con el medio ambiente.", R.drawable.patines));
-        listaONG.add(new ONG("Bicicletas Eléctricas", "Cabe destacar que permiten subir fácilmente terrenos con un gran desnivel, además de fomentar la práctica deportiva.", R.drawable.bicicleta));
-        listaONG.add(new ONG("Tablets", "El premio que puede ayudarte a estar conectado con las personas que deseas y claro, poder descargar nuestra app AQP Green.", R.drawable.tablet));
+        listaONG.add(new ONG("Ecomar Fundacion", "Fomento de la Responsabilidad Social Empresarial a través del Voluntariado Corporativo junto a los ODS de la Fundación ECOMAR..", R.drawable.ong1));
+        listaONG.add(new ONG("SERFOR", "El Servicio Nacional Forestal y de Fauna Silvestre (SERFOR).", R.drawable.ong2));
+        listaONG.add(new ONG("UICN", "Unión Internacional para la Conservación de la Naturaleza y de los Recursos Naturales y al Fondo Mundial para la Naturaleza.", R.drawable.ong3));
+        listaONG.add(new ONG("RAP", "RAP – Red Ambiental Peruana, contribuye al cuidado de los Recursos Naturales del Peru.", R.drawable.ong4));
+        listaONG.add(new ONG("ARA", "La Articulación Regional Amazónica – ARA está empezando un nuevo momento en su comunicación trabajando para perfeccionar nuestra red.", R.drawable.ong5));
+        listaONG.add(new ONG("Comité de Apoyo Parque Nacional Sierra del Divisor", "Protección de una muestra representativa de la región montañosa del bosque húmedo tropical del llano amazónico.", R.drawable.ong6));
     }
 
     private void inicializar_elementos (View view) {
@@ -85,7 +80,7 @@ public class ListaDeOng extends Fragment {
         recyclerONG.setAdapter(adapter);
 
         adapter.setOnClickListener(v -> {
-            Toast.makeText(getContext(),"Seleccion: "+
+            Toast.makeText(getContext(),"Seleccion de ONG: "+
                     listaONG.get(recyclerONG.
                             getChildAdapterPosition(v)).getNombre(),Toast.LENGTH_SHORT).show();
 
