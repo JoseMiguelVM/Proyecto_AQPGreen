d78531ef06b57935dcab24b6783e7b1db151f92f
Jose Antonio <sawm5798@gmail.com>
2022-12-23 09:22:04 -0500

    Actualizada vista calculo de huella

--- app/src/main/java/com/example/aqpgreen/ui/HuellaPlastico/HuellaPlasticoFragment.java
+++ app/src/main/java/com/example/aqpgreen/ui/HuellaPlastico/HuellaPlasticoFragment.java
@@ -22,47 +22,21 @@ import android.widget.ImageView;
 
 import com.example.aqpgreen.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HuellaPlasticoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HuellaPlasticoFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private Button button_calcular;
     private ImageButton btn_regresar_fragment;
-    private EditText BotePET,BotePlastic,EnvAlim,EnvaYogurt,EnvaLimpieza,EnvaCosmetic,CepDientes,PastaDental,VasoPlastico,OtroPlastic;
+    private TextInputLayout BotePET,
+            BotePlastic, EnvAlim,
+            EnvaYogurt, EnvaLimpieza,
+            EnvaCosmetic, CepDientes,
+            PastaDental, VasoPlastico,
+            OtroPlastic;
 
     public HuellaPlasticoFragment() {
 
-    }
-
-    public static HuellaPlasticoFragment newInstance(String param1, String param2) {
-        HuellaPlasticoFragment fragment = new HuellaPlasticoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -79,42 +53,31 @@ public class HuellaPlasticoFragment extends Fragment {
 
         inicializar_elementos(view);
 
-        btn_regresar_fragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.popBackStack();
-            }
-        });
+        btn_regresar_fragment.setOnClickListener(v -> navController.popBackStack());
 
-        button_calcular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //navController.navigate(R.id.);
-                Log.d("TAG","Hicimos click en el botton CALCULAR BOTELLA");
+        button_calcular.setOnClickListener(v -> {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                //AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Tu huella de plástico es: ");
-                double kgDeHuellaPlastica = CalculandHuellaPlastica();
-                String mensaje = "Utilizarás "+String.valueOf(kgDeHuellaPlastica)+" kg. de plástico a lo largo de tu vida.";
-                builder.setMessage(mensaje);
+            AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+            builder.setTitle("Tu huella de plástico es: ");
+            double kgDeHuellaPlastica = CalculandHuellaPlastica();
+            String mensaje = "Utilizarás "+String.valueOf(kgDeHuellaPlastica)+" kg. de plástico a lo largo de tu vida.";
+            builder.setMessage(mensaje);
 
 
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User clicked OK button
 
-                    }
-                });
-                builder.setNegativeButton("SALIR", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
-                    }
-                });
-                builder.create().show();
+                }
+            });
+            builder.setNegativeButton("SALIR", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                }
+            });
+            builder.create().show();
 
-
-            }
         });
 
     }
@@ -123,7 +86,7 @@ public class HuellaPlasticoFragment extends Fragment {
 
         button_calcular = view.findViewById(R.id.buttonCalcularHuellaPlastica);
         BotePET = view.findViewById(R.id.editTextNumBotellasPet);
-        BotePlastic = view.findViewById(R.id.editTextNumBotellasPet);
+        BotePlastic = view.findViewById(R.id.editTextNumBolsasPlastic);
         EnvAlim = view.findViewById(R.id.editTextNumEnvolAlim);
         EnvaYogurt = view.findViewById(R.id.editTextNumEnvaYogurt);
         EnvaLimpieza = view.findViewById(R.id.editTextNumEnvaLimpieza);
@@ -133,22 +96,41 @@ public class HuellaPlasticoFragment extends Fragment {
         VasoPlastico = view.findViewById(R.id.editTextNumVasosPlastico);
         OtroPlastic = view.findViewById(R.id.editTextNumKgDecimal);
 
-
         btn_regresar_fragment = view.findViewById(R.id.btnIcoAtras);
 
     }
     public Double CalculandHuellaPlastica(){
 
-        double botePET= (BotePET.getText().toString().isEmpty()) ? 0: Double.parseDouble(BotePET.getText().toString());
-        double botePlastic = (BotePlastic.getText().toString().isEmpty())?0:Double.parseDouble(BotePlastic.getText().toString());
-        double envAlim = (EnvAlim.getText().toString().isEmpty())?0:Double.parseDouble(EnvAlim.getText().toString());
-        double envaYogurt = (EnvaYogurt.getText().toString().isEmpty())?0:Double.parseDouble( EnvaYogurt.getText().toString());
-        double envaLimpieza = (EnvaLimpieza.getText().toString().isEmpty())?0:Double.parseDouble(EnvaLimpieza.getText().toString());
-        double envaCosmetic = (EnvaCosmetic.getText().toString().isEmpty())?0:Double.parseDouble( EnvaCosmetic.getText().toString());
-        double cepDientes = (CepDientes.getText().toString().isEmpty())?0:Double.parseDouble(CepDientes.getText().toString());
-        double pastaDental = (PastaDental.getText().toString().isEmpty())?0:Double.parseDouble(PastaDental.getText().toString());
-        double vasoPlastico = (VasoPlastico.getText().toString().isEmpty())?0:Double.parseDouble(VasoPlastico.getText().toString());
-        double otroPlastic = (OtroPlastic.getText().toString().isEmpty())?0:Double.parseDouble(OtroPlastic.getText().toString());
+        double botePET= (BotePET.getEditText().getText().toString().isEmpty())
+                ? 0
+                : Double.parseDouble(BotePET.getEditText().getText().toString());
+        double botePlastic = (BotePlastic.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(BotePlastic.getEditText().getText().toString());
+        double envAlim = (EnvAlim.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(EnvAlim.getEditText().getText().toString());
+        double envaYogurt = (EnvaYogurt.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble( EnvaYogurt.getEditText().getText().toString());
+        double envaLimpieza = (EnvaLimpieza.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(EnvaLimpieza.getEditText().getText().toString());
+        double envaCosmetic = (EnvaCosmetic.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble( EnvaCosmetic.getEditText().getText().toString());
+        double cepDientes = (CepDientes.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(CepDientes.getEditText().getText().toString());
+        double pastaDental = (PastaDental.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(PastaDental.getEditText().getText().toString());
+        double vasoPlastico = (VasoPlastico.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(VasoPlastico.getEditText().getText().toString());
+        double otroPlastic = (OtroPlastic.getEditText().getText().toString().isEmpty())
+                ? 0
+                :Double.parseDouble(OtroPlastic.getEditText().getText().toString());
 
         double calculandoHuellaSemanal = (
                 (botePET*0.036)
